6d9f325702d92cbfc268554f92f39bf61a946256
usmanrajput9990 <usmanrajput9990@gmail.com>
2025-04-30 14:48:18 +0500

    again test

--- next.config.js
+++
@@ -1,6 +0,0 @@
-const withNetlify = require('@netlify/plugin-nextjs');
-
-module.exports = withNetlify({
-  reactStrictMode: true,
-  // Other configurations
-});
---
+++ next.config.mjs
@@ -0,0 +1,6 @@
+const { withNetlify } = require('@netlify/plugin-nextjs');
+
+module.exports = {
+  reactStrictMode: true,
+  // Netlify-specific configuration if needed
+};
